be1bbc504f8478813f0f689e3297f7558ab38a4c
Elliot Fraser <e.fraser3@gmail.com>
2023-10-12 18:45:54 +0100

    Finalised logic and added basic styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 import Article from './Components/Article';
 
 function App() {
   const [articles, setArticles] = useState([]);
-  const [subreddit, setSubreddit] = useState(['webdev']);
+  const [subreddit, setSubreddit] = useState(['all']);
 
   useEffect(() => {
-    fetch("https://www.reddit.com/r/webdev.json").then(res => {
-      if (res.status != 200) {
-        console.log("ERROR!");
-        return;
-      }
+    const getData = setTimeout(() => {
+      let tempSubreddit = subreddit === "" ? 'all' : subreddit;
 
-      res.json().then(data => {
-        if (data != null) {
-          setArticles(data.data.children);
+      fetch("https://www.reddit.com/r/" + tempSubreddit + ".json").then(res => {
+        if (res.status != 200) {
+          console.log("ERROR!");
+          return;
         }
+  
+        res.json().then(data => {
+          if (data != null) {
+            setArticles(data.data.children);
+          }
+        })
       })
-    })
+    }, 2000);
+
+    return () => clearTimeout(getData);
   }, [subreddit]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <input type="text" className="input" value="webdev" />
+        <p>Snoo Surf</p>
+        <input type="text" className="input" value={subreddit} onChange={ (e) => setSubreddit(e.target.value)} />
       </header>
       <div className="articles">
         {
--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Article.css';
 
 function Article(props) {
     return (
         <article>
             <a href={ "https://reddit.com" + props.article.permalink } target="_blank">
                 <h3>{ props.article.title }</h3>
+                <img src={ props.article.thumbnail } />
+                <h4>{ props.article.ups } Upvotes</h4>
+                <h4>Posted by { props.article.author }</h4>
+                <h4>{ props.article.num_comments } comments</h4>
             </a>
         </article>
     )
