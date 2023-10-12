34adff0dc11e6b055d4ed77e3b363d538f79bc2d
Elliot Fraser <e.fraser3@gmail.com>
2023-10-12 16:47:08 +0100

    Integrated NavBar component into App to simplify logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
-import NavBar from './Features/NavBar/NavBar';
+import React, { useState, useEffect } from 'react';
+import Article from './Components/Article';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState(['webdev']);
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/webdev.json").then(res => {
+      if (res.status != 200) {
+        console.log("ERROR!");
+        return;
+      }
+
+      res.json().then(data => {
+        if (data != null) {
+          setArticles(data.data.children);
+        }
+      })
+    })
+  }, [subreddit]);
+
   return (
     <div className="App">
-      <NavBar />
+      <header className="App-header">
+        <input type="text" className="input" value="webdev" />
+      </header>
+      <div className="articles">
+        {
+          (articles != null) ? articles.map((article, index) => <Article key={index} article={article.data} />) : ''
+        }
+      </div>
     </div>
   );
 }
